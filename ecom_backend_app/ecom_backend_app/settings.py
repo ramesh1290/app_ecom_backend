cefde3cfb2f492b02cfba34df6ef6fabc2c7b915
ramesh1290 <krrishchhetri6@gmail.com>
2026-05-03 11:16:10 +0545

    fix:setting fixed

--- ecom_backend_app/ecom_backend_app/settings.py
+++ ecom_backend_app/ecom_backend_app/settings.py
@@ -156,10 +156,11 @@ STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 # EMAIL (OTP SYSTEM FIX)
 # ========================
 
-EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False 
 
 EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
